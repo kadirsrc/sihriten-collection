5f3d7b91a94134123b4d3b1c573d0d50453100fb
kadirsrc <50583390+kadirsrc@users.noreply.github.com>
2025-07-07 22:23:49 +0300

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,10 @@
 	import Counter from '$lib/Counter.svelte';
 </script>
 
-
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
 
 <section>
 	<h1>
